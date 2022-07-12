ad8f828ecad02f0a35399572901ce7cf78f421ee
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-07-12 11:18:47 +0200

    init

---
+++ src/main/java/fr/diginamic/javaFS2022/jpa/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.javaFS2022.jpa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
