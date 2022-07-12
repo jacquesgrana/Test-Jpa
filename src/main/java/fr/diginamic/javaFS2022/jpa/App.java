e6866479d4918ad179315d8608cd71e6d2b0feb4
Jacques Granarolo <jac.granarolo.info@gmail.com>
2022-07-12 11:53:13 +0200

    TP1 JPA

--- src/main/java/fr/diginamic/javaFS2022/jpa/App.java
+++ src/main/java/fr/diginamic/javaFS2022/jpa/App.java
@@ -1,13 +1,21 @@
 package fr.diginamic.javaFS2022.jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
- * Hello world!
+ * 
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa_local");
+    	EntityManager em = emf.createEntityManager();
+    	System.out.println("connection ok : " + em);
+    	em.close();
+    	emf.close();
     }
 }
